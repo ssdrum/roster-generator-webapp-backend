9d1d2acbe0e1b600f1d1b04e0ccf80dc894ef69f
ssdrum <luigi.dipaolo02@gmail.com>
2024-01-18 17:07:02 +0000

    added /api endpoint and added test branch preview url to test

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://roster-generator-webapp-git-development-api-in-9113f2-sdrummolo.vercel.app",
+]
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api")
 async def root():
     return {"message": "Hello World"}
