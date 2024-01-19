a8c925463caf223fe01280eede25231cbd49d11e
ssdrum <luigi.dipaolo02@gmail.com>
2024-01-19 17:01:36 +0000

    Limited solver to find a fixed amount of solutions, general improvements

--- algorithm.py
+++ algorithm.py
@@ -2,24 +2,28 @@ from ortools.sat.python import cp_model
 
 
 class SolutionPrinter(cp_model.CpSolverSolutionCallback):
-    """Stores intermediate solutions"""
+    """Stores solutions"""
 
-    def __init__(self, roster, e, d, s):
+    def __init__(self, roster, e, d, s, limit):
         cp_model.CpSolverSolutionCallback.__init__(self)
-        self.roster = roster
-        self.e = e
-        self.d = d
-        self.s = s
+        self.__roster = roster
+        self.__e = e
+        self.__d = d
+        self.__s = s
+        self.__limit = limit
         self.solutions = []
 
     def on_solution_callback(self):
         # Store all solutions in a list
         solution = {}
-        for i in range(1, self.e + 1):
-            for j in range(1, self.d + 1):
-                for k in range(1, self.s + 1):
-                    solution[(i, j, k)] = self.Value(self.roster[(i, j, k)])
+        for i in range(1, self.__e + 1):
+            for j in range(1, self.__d + 1):
+                for k in range(1, self.__s + 1):
+                    solution[(i, j, k)] = self.Value(self.__roster[(i, j, k)])
         self.solutions.append(solution)
+        if len(self.solutions) >= self.__limit:
+            print(f"Stopped searching after {self.__limit} solutions.")
+            self.StopSearch()
 
 
 def main():
@@ -27,8 +31,8 @@ def main():
     model = cp_model.CpModel()
 
     # Define variables
-    e = 2  # Number of employees
-    d = 7  # Number of days in the workw eek
+    e = 1  # Number of employees
+    d = 3  # Number of days in the work week
     s = 3  # Number of shifts
 
     employees_range = range(1, e + 1)
@@ -48,29 +52,38 @@ def main():
         for j in days_range:
             model.AddExactlyOne(roster[(i, j, k)] for k in shifts_range)
 
+    # 2. TODO Each employee works at most 5 shift per week
+
+    # 3. TODO Each shift must be covered by one employee
+
     # Solve
     solver = cp_model.CpSolver()
-    solution_printer = SolutionPrinter(roster, e, d, s)
+    solution_printer = SolutionPrinter(roster, e, d, s, 5)
     # Find solutions
+    solver.parameters.enumerate_all_solutions = True
     status = solver.Solve(model, solution_printer)
 
     # Print solutions
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         print()
         days = ["M", "T", "W", "T", "F", "S", "S"]
-        header = ""
-        for d in days_range:
-            header += f"{days[d - 1]} "
-        print(header)
-        sol_1 = solution_printer.solutions[0]
-        i = 1
-        for shift in sol_1:
-            if sol_1[shift] == 1:
-                print(f"{shift[2]} ", end="")
-                if i % d == 0:
-                    print()
-                i += 1
+        for solution in solution_printer.solutions:
+            header = ""
+            for d in days_range:
+                header += f"{days[d - 1]} "
+            print(header)
+            i = 1
+            for shift in solution:
+                if solution[shift] == 1:
+                    print(f"{shift[2]} ", end="")
+                    if i % d == 0:
+                        print()
+                    i += 1
+            print()
 
+    assert len(solution_printer.solutions) == 5
+
+    # Print statistics
     print("Statistics")
     print("  - status          : %s" % solver.StatusName(status))
     print("  - conflicts       : %i" % solver.NumConflicts())
